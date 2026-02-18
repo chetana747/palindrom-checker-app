38ec36ca8f3ce45767ba3c2820a600f62839d29f
chetana747 <chetss747@gmail.com>
2026-02-18 15:30:58 +0530

    UC1: Add welcome message module

---
+++ src/PalindromCheckerApp.java
@@ -0,0 +1,8 @@
+public class PalindromCheckerApp {
+    public static void main(String[] args) {
+
+        System.out.println("welcome to palindrom checker app");
+        System.out.println("version 1.0");
+        System.out.println("system initialized successfully");
+    }
+}
