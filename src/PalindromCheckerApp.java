bf2f2350d37412e9c780d284fbfcaf3adc843ce5
chetana747 <chetss747@gmail.com>
2026-02-25 14:30:18 +0530

    UC2:Hardcoded palindrome

--- src/PalindromCheckerApp.java
+++ src/PalindromCheckerApp.java
@@ -1,8 +1,25 @@
-public class PalindromCheckerApp {
+import java.util.Scanner;
+
+public class UseCase2PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
-        System.out.println("welcome to palindrom checker app");
-        System.out.println("version 1.0");
-        System.out.println("system initialized successfully");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String input = sc.nextLine();
+
+        String original = input.toLowerCase();
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        boolean isPalindrome = original.equals(reversed);
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
